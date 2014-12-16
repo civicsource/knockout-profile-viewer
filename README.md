8d6fe4189e791e46fceac05285e764a0fdd8fb85
Djihbrihou Abibou <dj.abibou@archoninfosys.com>
2014-12-16 12:31:12 -0600

    Componentizing the CivicSource profile viewer.

---
+++ README.md
@@ -0,0 +1,23 @@
+#Knockout Profile Viewer
+
+> Knockout component to view a CivicSource Administrator or Auctioneer Profile.
+
+##Install with [Bower](http://bower.io/)
+
+```
+bower install civicsource/knockout-profile-viewer --save
+```
+
+Then add the folder to your project.
+
+##How to Use
+
+`require` the script and use it as a binding handler:
+
+```html
+<span data-bind="profileViewer: myProfile"></span>
+```
+
+where `myProfile` is an observable you want two-way bound to the component. Its value can be either an object with a username property, or a string representing the username.
+
+If the bound profile does not have a `fullName` property, it will hit the server to retrieve the full profile object and populate the observable. Otherwise, it will use what is passed in to show the profile's full name.
---
+++ knockout-profile-viewer.js
@@ -0,0 +1,47 @@
+﻿(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define(["jquery", "knockout", "app/urls"], factory);
+  } else {
+    // Browser globals
+    factory(jQuery, ko);
+  }
+}(this, function ($, ko, urls) {
+  ko.bindingHandlers.profileViewer = {
+    // takes a username as the value and returns a profile object
+    update: function (el, valueAccessor, allBindings) {
+      var profile = ko.utils.unwrapObservable(valueAccessor());
+
+      if (!profile) {
+        el.innerHTML = "No profile provided";
+      } else {
+        if (profile.fullName) {
+          el.innerHTML = profile.fullName;
+        } else {
+          var url = urls.civicsource + "profiles/" + (profile.username ? profile.username : profile);
+          $.ajax(url, {
+            type: "GET",
+            contentType: "application/json",
+          }).then(function (data) {
+            var obs = valueAccessor();
+
+            if (ko.isObservable(obs)) {
+              var prop;
+              for (prop in data) {
+                if (obs.hasOwnProperty(prop) && ko.isWriteableObservable(obs[prop])) {
+                  obs[prop](data[prop]);
+                }
+
+                else {
+                  obs[prop] = ko.observable(data[prop]);
+                }
+              }
+            }
+
+            el.innerHTML = data.fullName;
+          });
+        }
+      }
+    }
+  };
+}));
