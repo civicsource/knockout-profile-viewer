b73a4c49260443c3180cb08dafedde3438992448
Chad Lee <chad.lee@archoninfosys.com>
2014-12-23 09:23:29 -0600

    Fixing URL for admin profile API endpoint

--- knockout-profile-viewer.js
+++ knockout-profile-viewer.js
@@ -18,7 +18,7 @@
 				if (profile.fullName) {
 					el.innerHTML = profile.fullName;
 				} else {
-					var url = urls.admin + "api/profiles/" + (profile.username ? profile.username : profile);
+					var url = urls.admin + "profiles/" + (profile.username ? profile.username : profile);
 					$.ajax(url, {
 						type: "GET",
 						contentType: "application/json",
